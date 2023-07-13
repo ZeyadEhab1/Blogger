df5c2c1f5e8ee349e0426af68c5881bb41a6b88e
zeyad ehab <107952157+ZeyadEhab1@users.noreply.github.com>
2023-07-13 08:36:55 +0300

    Add files via upload

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
+            <h1 class="text-center font-bold text-xl">Register</h1>
+            <form method="Post" action="/register" class="mt-10">
+                @csrf
+                <x-form.input name="name"/>
+                <x-form.input name="username"/>
+                <x-form.input name="email" type="email" autocomplete="username"/>
+                <x-form.input name="password" type="password"/>
+                <x-form.button>Submit</x-form.button>
+            </form>
+        </main>
+    </section>
+</x-layout>
